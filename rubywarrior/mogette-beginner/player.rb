68c0f7577ee3b4db4bb90b7aacfa4c759aab43d0
Shakadak <nathanael.pineau@gmail.com>
2015-02-10 11:43:27 +0100

    level 7\?

--- rubywarrior/mogette-beginner/player.rb
+++ rubywarrior/mogette-beginner/player.rb
@@ -1,14 +1,25 @@
 class Player
-  def play_turn(warrior)
-	  if warrior.health < 20 && @prev_health == warrior.health && warrior.feel.empty?
-		  puts "before rest"
-		  warrior.rest!
-		  puts "after rest"
-	  elsif warrior.feel.empty?
-		  warrior.walk!
-	  else
-		  warrior.attack!
-	  end
-	  @prev_health = warrior.health
-  end
+
+	def initialize()
+		@max_health = 0
+		@expected_health = 0
+	end
+
+	def play_turn(warrior)
+		heal = 0
+		if @max_health == 0
+			@max_health = warrior.health
+		end
+		if warrior.health < @max_health && @expected_health == warrior.health && warrior.feel.empty?
+			warrior.rest!
+			heal = @max_health * 0.1
+		elsif warrior.feel.enemy?
+			warrior.attack!
+		elsif warrior.feel.captive?
+			warrior.rescue!
+		else
+			warrior.walk!
+		end
+		@expected_health = warrior.health + heal
+	end
 end
